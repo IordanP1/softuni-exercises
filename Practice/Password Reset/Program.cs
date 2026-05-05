318d5fd62c3893640aa2521083a44da749497d3c
Yordan Penkov <92777926+IordanP1@users.noreply.github.com>
2026-05-05 15:37:49 +0300

    Add files via upload

---
+++ Practice/Password Reset/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Password_Reset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count  = int.Parse(Console.ReadLine());
+            string pattern = @"@#+[A-Z][A-Za-z0-9]{4,}[A-Z]@#+";
+            for (int i = 0; i < count; i++)
+            {
+                string barcode = Console.ReadLine();
+                if (Regex.IsMatch(barcode, pattern))
+                {
+                    char[] digits = barcode.Where(c => char.IsDigit(c)).ToArray();
+                    string product = digits.Length > 0
+                        ? new(digits)
+                        : "00";
+
+                    Console.WriteLine($"Product group: {product}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                    continue;
+                }
+            }
+        }
+    }
+}
+/*
+6
+@###Val1d1teM@###
+@#ValidIteM@#
+##InvaliDiteM##
+@InvalidIteM@
+@#Invalid_IteM@#
+@#ValiditeM@#
+ */
